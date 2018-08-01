ec52b2c4ffd49dad57e292c39b15af25de5594e7
Stephen <stephen@whitebolt.net>
2018-08-01 22:04:17 +0100

    Refactor to provide a registry
    Created a gulp4 style registry that is exported. If new is not applied then assume not registry.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 'use strict';
 
-module.exports = require('./lib');
+const Undertaker = require('undertaker-registry');
+const {importTasks, set, get} = require('./lib');
+
+const restictedProps = new Set(['src', 'dest', 'symlink', 'task', 'lastRun', 'parallel', 'series', 'watch', 'tree', 'registry']);
+
+
+class _Import_Tasks extends Undertaker {
+	constructor(options={}) {
+		super(options);
+		Object.keys(options).forEach(key=>{
+			if (!restictedProps.has(key)) this[key] = options[key];
+		});
+	}
+
+	init(undertaker) {
+		importTasks(this, undertaker);
+	}
+}
+
+function Import_Tasks(...params) {
+	if (new.target)	{
+		return new _Import_Tasks(...params);
+	} else{
+		return importTasks(...params)
+	}
+}
+
+Import_Tasks.set = set;
+Import_Tasks.get = get;
+
+module.exports = Import_Tasks;
